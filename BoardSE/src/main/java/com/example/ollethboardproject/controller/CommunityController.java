6879456637750e0783f85b7b54373bc268d988c0
박지영 <zhsclqzlffj2@gmail.com>
2023-05-02 16:31:41 +0900

    #5Community change

---
+++ BoardSE/src/main/java/com/example/ollethboardproject/controller/CommunityController.java
@@ -0,0 +1,68 @@
+package com.example.ollethboardproject.controller;
+
+import com.example.ollethboardproject.controller.request.CommunityCreateRequest;
+import com.example.ollethboardproject.controller.request.CommunityUpdateRequest;
+import com.example.ollethboardproject.domain.dto.CommunityDTO;
+import com.example.ollethboardproject.service.CommunityService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/communities")
+public class CommunityController {
+    private final CommunityService communityService;
+
+    // 커뮤니티 전체 조회
+    @GetMapping("")
+    public ResponseEntity<List<CommunityDTO>> findAllCommunity() {
+        log.info("GET /api/v1/communities");
+        List<CommunityDTO> communityDTOList = communityService.findAllCommunities();
+        return new ResponseEntity<>(communityDTOList, HttpStatus.OK);
+    }
+
+    //커뮤니티 생성
+    @PostMapping("")
+    public ResponseEntity<CommunityDTO> createCommunity(@RequestBody CommunityCreateRequest communityCreateRequest, Authentication authentication) {
+        log.info("POST /api/v1/communities");
+        log.info("CommunityCreateRequest & authentication: {}, {} ", communityCreateRequest, authentication);
+        CommunityDTO createdCommunityDTO = communityService.createCommunity(communityCreateRequest, authentication);
+        log.info("createdCommunityDTO : {}", createdCommunityDTO);
+        return new ResponseEntity<>(createdCommunityDTO, HttpStatus.CREATED);
+    }
+
+    //커뮤니티 정보 수정
+    //TODO: LIST -> id 외 조회 기준 추가
+    @PutMapping("/{id}")
+    public ResponseEntity<CommunityDTO> updateCommunity(@PathVariable Long id, @RequestBody CommunityUpdateRequest communityUpdateRequest , Authentication authentication) {
+        log.info("PUT /api/v1/communities/{}", id);
+        CommunityDTO updatedCommunityDTO = communityService.updateCommunity(id, communityUpdateRequest, authentication);
+        return new ResponseEntity<>(updatedCommunityDTO, HttpStatus.OK);
+    }
+
+    //커뮤니티 삭제
+    //TODO: LIST -> id 외 조회 기준 추가
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id, Authentication authentication) {
+        log.info("DELETE /api/v1/communities/{}", id);
+        communityService.deleteBoard(id, authentication);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    //커뮤니티 가입
+    //TODO: LIST -> 관리자 승인 및 취소 기능 추가
+
+    //커뮤니티 기가입 멤버 조회
+
+    //관심사&키워드 기반 커뮤니티 추천 기능
+    //TODO: ENUM 사용
+
+
+}
--- BoardSE/src/main/java/com/example/ollethboardproject/service/MemberService.java
+++ BoardSE/src/main/java/com/example/ollethboardproject/service/MemberService.java
@@ -1,19 +1,25 @@
 package com.example.ollethboardproject.service;
 
+import com.example.ollethboardproject.controller.request.MemberUpdateRequest;
 import com.example.ollethboardproject.controller.request.MemberJoinRequest;
 import com.example.ollethboardproject.controller.request.MemberLoginRequest;
+import com.example.ollethboardproject.controller.request.PwEncodeRequest;
 import com.example.ollethboardproject.domain.dto.MemberDTO;
 import com.example.ollethboardproject.domain.entity.Member;
+import com.example.ollethboardproject.domain.entity.Post;
 import com.example.ollethboardproject.exception.BoardException;
 import com.example.ollethboardproject.exception.ErrorCode;
+import com.example.ollethboardproject.repository.CommunityRepository;
 import com.example.ollethboardproject.repository.MemberRepository;
 import com.example.ollethboardproject.repository.PostRepository;
 import com.example.ollethboardproject.repository.OllehRepository;
+import com.example.ollethboardproject.utils.ClassUtil;
 import com.example.ollethboardproject.utils.JwtTokenUtil;
 import com.example.ollethboardproject.utils.TokenInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -46,11 +52,15 @@ public class MemberService implements UserDetailsService {
                     throw new BoardException(ErrorCode.DUPLICATED_USERNAME, String.format("%s is duplicated", memberJoinRequest.getUserName()));
                 });
         //TODO: 비밀번호 제약조건 설정 여부
-        //member 비밀번호 암호화 후 엔티티 생성
-        Member member = Member.of(encodePassword(memberJoinRequest));
+        //비밀번호 암호화를 위해 pwEncodeRequest 타입으로 변환
+        PwEncodeRequest pwEncodeRequest = new PwEncodeRequest(
+                memberJoinRequest.getUserName(), memberJoinRequest.getNickName(), memberJoinRequest.getPassword(),
+                memberJoinRequest.getGender(), memberJoinRequest.getRoles());
+
+        //비밀번호 암호화 후 member 타입으로 객체 생성
+        Member member = Member.toPw(encodePassword(pwEncodeRequest));
         //member 엔티티 저장
         Member savedMember = memberRepository.save(member);
-        log.info("saveMember : {}",savedMember);
         //entity -> DTO 로 변환후 return
         return MemberDTO.fromEntity(savedMember);
     }
@@ -72,11 +82,64 @@ public class MemberService implements UserDetailsService {
         return TokenInfo.generateTokens(accessToken, refreshToken);
     }
 
-    private MemberJoinRequest encodePassword(MemberJoinRequest memberJoinRequest) {
+    private PwEncodeRequest encodePassword(PwEncodeRequest pwEncodeRequest) {
         //비밀번호 암호화
-        String encodePassword = encoder.encode(memberJoinRequest.getPassword());
-        memberJoinRequest.encode(encodePassword);
-        return memberJoinRequest;
+        String encodePassword = encoder.encode(pwEncodeRequest.getPassword());
+        pwEncodeRequest.encode(encodePassword);
+        return pwEncodeRequest;
+    }
+
+    public MemberDTO findMemberByPassword(String password, Authentication authentication) {
+        //캐스팅에 의한 에러가 나지 않도록 ClassUtil 메서드 사용
+        Member member = ClassUtil.castingInstance(authentication.getPrincipal(), Member.class).get();
+        //비밀번호가 일치하는 회원 정보를 조회할 수 있다.
+        passwordMatches(member, password);
+        //entity -> DTO 로 변환후 return
+        return MemberDTO.fromEntity(member);
+    }
+
+    public MemberDTO updateMember(String password, MemberUpdateRequest memberUpdateRequest, Authentication authentication) {
+        //캐스팅에 의한 에러가 나지 않도록 ClassUtil 메서드 사용
+        Member member = ClassUtil.castingInstance(authentication.getPrincipal(), Member.class).get();
+        //수정된 회원 정보 중복 검증
+        duplicationMatches(member.getUsername());
+        //비밀번호가 일치하는 회원만 회원 정보를 수정할 수 있다.
+        passwordMatches(member, password);
+        //수정된 비밀번호 암호화
+        PwEncodeRequest pwEncodeRequest = new PwEncodeRequest(
+                memberUpdateRequest.getUserName(), memberUpdateRequest.getNickName(), memberUpdateRequest.getPassword(),
+                memberUpdateRequest.getGender(), memberUpdateRequest.getRole());
+        //암화화된 비밀번호가 포함된 정보를 member 타입으로 객체화
+        Member updatedMember = Member.toPw(encodePassword(pwEncodeRequest));
+        //회원 정보 수정 (Setter 를 사용하지 않기 위함)
+        member.update(updatedMember);
+        //수정된 회원 정보 저장
+        memberRepository.save(member);
+        return MemberDTO.fromEntity(updatedMember);
+    }
+
+    public void deleteMember(String password, Authentication authentication) {
+        //캐스팅에 의한 에러가 나지 않도록 ClassUtil 메서드 사용
+        Member member = ClassUtil.castingInstance(authentication.getPrincipal(), Member.class).get();
+        //비밀번호가 일치하는 회원만 회원 정보를 삭제할 수 있다.
+        passwordMatches(member, password);
+        //회원 정보 삭제
+        memberRepository.deleteById(member.getId());
+    }
+
+    //비밀번호 일치 검증 메서드
+    private void passwordMatches(Member member, String password) {
+        //비교할 password 암호화
+        String encodePassword = encoder.encode(password);
+        if (encoder.matches(encodePassword, member.getPassword())) {
+            throw new BoardException(ErrorCode.HAS_NOT_PERMISSION_TO_ACCESS);
+        }
+    }
+
+    //수정된 회원 정보 중복 검증 메서드
+    private void duplicationMatches(String updaterName) {
+        memberRepository.findByUserName(updaterName).orElseThrow(() ->
+                new BoardException(ErrorCode.DUPLICATED_USERNAME, String.format("%s not found", updaterName)));
     }
 
     @Override
@@ -84,4 +147,5 @@ public class MemberService implements UserDetailsService {
         return memberRepository.findByUserName(username).orElseThrow(() ->
                 new BoardException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", username)));
     }
+
 }
